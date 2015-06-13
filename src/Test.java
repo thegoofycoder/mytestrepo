940d8cdb97d5301505922e0b2159938b3a0624cf
goofy <goofy@goofy>
2015-06-14 00:11:37 +0530

    my branch test

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-System.out.println("Test");
-System.out.println("Test");
-System.out.println("Test");
+
+System.out.println(" branch Test");
+System.out.println(" branch Test");
 
 
 	}
