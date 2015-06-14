f78c161e2e3829886f40a2143dfe1daabbb5bd29
venkadesh <venkadesh@venkadesh>
2015-06-14 16:41:39 +0530

    tesy with chages

--- src/Test.java
+++ src/Test.java
@@ -3,16 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
-System.out.println(" new branch Test");
+System.out.println(" new branch Test with changes");
+
 
 
 	}
